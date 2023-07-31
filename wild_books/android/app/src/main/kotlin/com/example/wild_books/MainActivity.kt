63d96a5596d7282aadd7a6e0a9783d4a27dfe696
yaahcd <cremaschi.yara@gmail.com>
2023-07-31 13:19:40 +0100

    git problems hope this works

---
+++ wild_books/android/app/src/main/kotlin/com/example/wild_books/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wild_books
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ wild_books/lib/add_a_book_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AddBook extends StatefulWidget {
+  const AddBook({super.key});
+
+  @override
+  State<AddBook> createState() => _AddBookState();
+}
+
+class _AddBookState extends State<AddBook> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add a book'),
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: const Icon(Icons.arrow_back_ios),
+        ),
+      ),
+    );
+  }
+}
