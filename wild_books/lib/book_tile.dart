f25fe525af9dc13286fd91099d2fba7ac693151b
Yara <cremaschi.yara@gmail.com>
2023-08-01 17:51:36 +0100

    made a few changes to home page

--- wild_books/lib/book_tile.dart
+++ wild_books/lib/book_tile.dart
@@ -8,36 +8,60 @@ class BookTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: ListTile(
-      leading: SizedBox(
-        width: 100,
-        height: 500,
-        child: Image.network(book.bookCover, fit: BoxFit.fill),
+      width: MediaQuery.of(context).size.width * 0.94,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5.0),
+          //side: BorderSide(color: Colors.grey)
+        ),
+        color: Colors.white,
+        elevation: 5,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(2.0),
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  maxWidth: MediaQuery.of(context).size.width * 0.30,
+                  maxHeight: MediaQuery.of(context).size.width * 0.30,
+                ),
+                child: Image.network(book.bookCover, fit: BoxFit.fill),
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.6,
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(30, 10, 0, 0),
+                    child: Text(
+                      'Title: ${book.title}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.6,
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(30, 10, 0, 0),
+                    child: Text(
+                      'Author: ${book.author}',
+                      style: TextStyle(
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
-      title: Text('Title: ${book.title}',
-          style: TextStyle(color: Colors.grey[600], fontSize: 20)),
-      subtitle: Text(
-        'Author: ${book.author}',
-        style: TextStyle(color: Colors.grey[600], fontSize: 15),
-      ),
-      // margin: EdgeInsets.only(left: 25),
-      // width: 200,
-      // decoration: BoxDecoration(
-      //     color: Colors.grey[100], borderRadius: BorderRadius.circular(12)),
-      // child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-      //   Image.asset(book.bookCover),
-      //   Row(
-      //     crossAxisAlignment: CrossAxisAlignment.end,
-      //     children: [
-      //       Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
-      //         Text('Title: ${book.title}',
-      //             style: TextStyle(color: Colors.grey[600], fontSize: 20)),
-      //         Text('Author: ${book.author}',
-      //             style: TextStyle(color: Colors.grey[600], fontSize: 15))
-      //       ])
-      //     ],
-      //   )
-      // ]),
-    ));
+    );
   }
 }
--- wild_books/lib/home_page.dart
+++ wild_books/lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:wild_books/book.dart';
 import 'package:wild_books/book_tile.dart';
 import 'package:wild_books/utils/db.dart';
@@ -13,59 +14,78 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(
-          height: 45,
-        ),
-        Container(
-          padding: const EdgeInsets.all(12),
-          margin: const EdgeInsets.symmetric(horizontal: 25),
-          decoration: BoxDecoration(
-              color: Colors.grey[200], borderRadius: BorderRadius.circular(8)),
-          child: const Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                'Insert your code here',
-                style: TextStyle(color: Colors.grey),
-              ),
-              Icon(
-                Icons.send,
-                color: Colors.grey,
-              )
-            ],
-          ),
-        ),
-        const Padding(
-          padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 20.0),
-          child: Text(
-              'Introducing Wild Books, a web-based application that fosters a reading community and promotes recycling in the most novel way possible. Read more here... ',
-              style: TextStyle(fontSize: 20)),
-        ),
-        const SizedBox(
-          height: 45,
-        ),
-        Expanded(
-            child: FutureBuilder(
-                future: getBooks(),
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData) {
-                    return const Center(child: CircularProgressIndicator());
-                  }
-                  final books = snapshot.data!;
-                  debugPrint(books.toString());
-                  return ListView.builder(
-                      itemCount: 10,
-                      itemBuilder: (context, index) {
-                        Book book = Book(
-                            title: books[index]['title'],
-                            author: books[index]['author'],
-                            bookCover: books[index]['image_url']);
-                        return BookTile(book: book);
-                      });
-                }))
-      ],
-    );
+    return Column(children: [
+      SizedBox(
+        height: 15,
+      ),
+      SingleChildScrollView(
+          child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextField(
+                        onChanged: (text) {
+                          print(text);
+                        },
+                        decoration: const InputDecoration(
+                            filled: true,
+                            hintText: 'Please enter your code here',
+                            border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(15.0)))),
+                      ),
+                      // Container(
+                      //   padding: const EdgeInsets.all(12),
+                      //   margin: const EdgeInsets.symmetric(horizontal: 25),
+                      //   decoration: BoxDecoration(
+                      //       color: Colors.grey[200], borderRadius: BorderRadius.circular(8)),
+                      //   child: const Row(
+                      //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      //     children: [
+                      //       Text(
+                      //         'Insert your code here',
+                      //         style: TextStyle(color: Colors.grey),
+                      //       ),
+                      //       Icon(
+                      //         Icons.send,
+                      //         color: Colors.grey,
+                      //       )
+                      //     ],
+                      //   ),
+                      // ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(
+                            vertical: 50.0, horizontal: 20.0),
+                        child: Text(
+                            'Introducing Wild Books, a web-based application that fosters a reading community and promotes recycling in the most novel way possible. Read more here... ',
+                            style: TextStyle(fontSize: 20)),
+                      ),
+                      Expanded(
+                          child: FutureBuilder(
+                              future: getBooks(),
+                              builder: (context, snapshot) {
+                                if (!snapshot.hasData) {
+                                  return const Center(
+                                      child: CircularProgressIndicator());
+                                }
+                                final books = snapshot.data!;
+                                debugPrint(books.toString());
+                                return ListView.builder(
+                                    itemCount: 10,
+                                    itemBuilder: (context, index) {
+                                      Book book = Book(
+                                          title: books[index]['title'],
+                                          author: books[index]['author'],
+                                          bookCover: books[index]['image_url']);
+                                      return BookTile(book: book);
+                                    });
+                              }))
+                    ],
+                  ))))
+    ]);
   }
 }
