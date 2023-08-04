eb990734fe4a7fa10e6a1cf13df402131b23dc44
yoosuf <yoosuf@Yoosuf>
2023-08-04 13:27:35 +0100

    model class for book events page made

---
+++ wild_books/lib/model/book_events.dart
@@ -0,0 +1,20 @@
+class SingleBookEvents {
+  final String title;
+  final String author;
+  final String bookCover;
+  final String event;
+  final DateTime timestamp;
+  final String nameOfEventStarter;
+  final String note;
+  final String comments;
+  SingleBookEvents({
+    required this.title,
+    required this.author,
+    required this.bookCover,
+    required this.event,
+    required this.timestamp,
+    required this.nameOfEventStarter,
+    required this.note,
+    required this.comments,
+  });
+}
