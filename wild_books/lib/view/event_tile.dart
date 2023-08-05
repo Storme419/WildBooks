fb0801cc52a6286da65f162de96f18c6aa771d0c
yoosuf <yoosuf@Yoosuf>
2023-08-05 13:17:31 +0100

    few changes to singlebook db function

---
+++ wild_books/lib/view/event_tile.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:wild_books/classes/single_book_event.dart';
+
+class EventTile {
+  SingleBookEvent singleBook;
+  EventTile({super.key, required this.singleBook});
+}
