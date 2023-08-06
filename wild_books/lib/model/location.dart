9551aaab6132d24499ae3a61ebd1e7fac441886b
yaahcd <cremaschi.yara@gmail.com>
2023-08-06 12:10:11 +0100

    reverse geocoding working and changed some style bits

--- wild_books/lib/model/location.dart
+++ wild_books/lib/model/location.dart
@@ -1,4 +1,23 @@
+import 'package:geocoding/geocoding.dart';
+
 class UserLocation {
-  static double lat = 52.2165157;
-  static double long = 6.9437819;
+  dynamic getLocation(dynamic lat, dynamic long) async {
+    try {
+      List<Placemark> placemark = await placemarkFromCoordinates(lat, long);
+
+      print(placemark[0].country);
+      print(placemark[0].locality);
+      print(placemark[0].administrativeArea);
+      print(placemark[0].postalCode);
+      print(placemark[0].name);
+      print(placemark[0].isoCountryCode);
+      print(placemark[0].subLocality);
+      print(placemark[0].subThoroughfare);
+      print(placemark[0].thoroughfare);
+
+      return placemark[0].administrativeArea;
+    } catch (e) {
+      print(e);
+    }
+  }
 }
