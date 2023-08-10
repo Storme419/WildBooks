164694538e340299e246f06a3485eabb78649ffe
yoosuf <yoosuf@Yoosuf>
2023-08-10 11:48:41 +0100

    added basic event and comment details

--- wild_books/lib/view/singleBook.dart
+++ wild_books/lib/view/singleBook.dart
@@ -159,165 +159,17 @@ class _SingleBookPageState extends State<SingleBookPage> {
                     //comments and event container
                     Container(
                       height: 1000,
-                      child: ListView.builder(
-                        itemCount: bookData.events.length,
-                        itemBuilder: (context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.all(15.0),
-                          child: Container(
-                            decoration: BoxDecoration(
-                              color: Colors.brown[50],
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            child: Column(
-                              children: [
-                                Container(
-                                  margin: EdgeInsets.only(
-                                      top: 5, left: 25, right: 25, bottom: 10),
-                                  padding: EdgeInsets.all(25),
-                                  child: Row(
-                                    children: [
-                                      Padding(
-                                        padding:
-                                            const EdgeInsets.only(bottom: 30),
-                                        child: Container(
-                                          decoration: BoxDecoration(
-                                            shape: BoxShape.circle,
-                                            color: Colors.grey[400],
-                                          ),
-                                          padding: EdgeInsets.all(10),
-                                          margin: EdgeInsets.only(
-                                            right: 15,
-                                          ),
-                                          child: const Icon(Icons.person),
-                                        ),
-                                      ),
-                                      Expanded(
-                                        child: Column(
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.start,
-                                          children: [
-                                            Text(
-                                              'Found or Released: ${bookData.events[0].event} by ${bookData.events[0].username}',
-                                              style: TextStyle(
-                                                  color: Colors.grey[500]),
-                                            ),
-                                            Text(
-                                              timeago.format(DateTime.parse(
-                                                  bookData.events[0].timestamp)),
-                                              style: TextStyle(
-                                                  color: Colors.grey[500]),
-                                            ),
-                                            const SizedBox(
-                                              height: 5,
-                                            ),
-                                            Text(
-                                              bookData.events[0].userNote,
-                                              textAlign: TextAlign.left,
-                                            ),
-                                          ],
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                ),
-                    
-                                //comments for event
-                                Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.brown[50],
-                                    borderRadius: BorderRadius.circular(8),
-                                  ),
-                                  margin: EdgeInsets.only(
-                                      top: 5, left: 25, right: 25, bottom: 10),
-                                  padding: EdgeInsets.all(25),
-                                  child: Column(
-                                    children: [
-                                      Padding(
-                                        padding:
-                                            const EdgeInsets.only(bottom: 20),
-                                        child: Text(
-                                          'Comments',
-                                          style: TextStyle(
-                                            fontSize: 20,
-                                            fontWeight: FontWeight.bold,
-                                          ),
-                                          textAlign: TextAlign.left,
-                                        ),
-                                      ),
-                                      Row(
-                                        children: [
-                                          Padding(
-                                            padding:
-                                                const EdgeInsets.only(bottom: 70),
-                                            child: Container(
-                                              decoration: BoxDecoration(
-                                                shape: BoxShape.circle,
-                                                color: Colors.grey[400],
-                                              ),
-                                              padding: EdgeInsets.all(10),
-                                              margin: EdgeInsets.only(
-                                                right: 15,
-                                              ),
-                                              child: const Icon(Icons.person),
-                                            ),
-                                          ),
-                                          Expanded(
-                                            child: Column(
-                                              crossAxisAlignment:
-                                                  CrossAxisAlignment.start,
-                                              children: [
-                                                Text(
-                                                  bookData.events[0].comments[0]
-                                                      .username,
-                                                  style: TextStyle(
-                                                    color: Colors.grey[500],
-                                                  ),
-                                                ),
-                                                const SizedBox(
-                                                  height: 5,
-                                                ),
-                                                Text(
-                                                  bookData.events[0].comments[0]
-                                                      .commentsBody,
-                                                ),
-                                                const SizedBox(
-                                                  height: 20,
-                                                ),
-                                                TextField(
-                                                    style: const TextStyle(
-                                                        fontSize: 15),
-                                                    onChanged: (text) {
-                                                      print(text);
-                                                    },
-                                                    decoration: InputDecoration(
-                                                      suffixIcon: IconButton(
-                                                        onPressed: () {},
-                                                        icon: Icon(Icons.send),
-                                                      ),
-                                                      hoverColor:
-                                                          Colors.blue[900],
-                                                      hintText: 'Post a comment',
-                                                      border: const OutlineInputBorder(
-                                                          borderRadius:
-                                                              BorderRadius.all(
-                                                                  Radius.circular(
-                                                                      15.0))),
-                                                    )),
-                                              ],
-                                            ),
-                                          )
-                                        ],
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        );
-                      }),
-                    )
+                      child: Column(children: [
+                        Text('Found or Released:${bookData.events[0].event}'),
+                        Text('${timeago.format(DateTime.parse(bookData.events[0].timestamp))}'),
+                        Text('Event note: ${bookData.events[0].userNote}'),
+                        Text('UserId for Event:${bookData.events[0].userId}'),
+                        Text('UserName for Event:${bookData.events[0].username}'),
+                        Text('UserName for Event:${bookData.events[0].event}'),
+                        Text('Comment on Event:${bookData.events[0].comments[0].commentsBody}'),
+                        Text('username of commenter:${bookData.events[0].comments[0].username}'),
+                      ]),
+                    ),
                   ]),
                 ),
               ),
