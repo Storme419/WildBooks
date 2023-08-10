dfcaf9f7f63b8d18926d2390d30a0052f0f13ffc
yoosuf <yoosuf@Yoosuf>
2023-08-10 15:01:58 +0100

    events page styled

--- wild_books/lib/view/event_tile.dart
+++ wild_books/lib/view/event_tile.dart
@@ -8,18 +8,19 @@ import 'package:timeago/timeago.dart' as timeago;
 import '../utils/db.dart';
 
 class EventTile extends StatefulWidget {
-  SingleBook singleBook;
-  SingleBookEvents singleBookEvent;
-  SingleBookEventComment singleBookEventComment;
+  // SingleBook singleBook;
+  // SingleBookEvents singleBookEvent;
+  // SingleBookEventComment singleBookEventComment;
 
-  // SingleBookData singleBook2;
+  SingleBookData singleBook2;
+  SingleBookEvent singleBookEvent;
+  SingleBookComment singleBookComment;
 
   EventTile({
     super.key,
-    required this.singleBook,
+    required this.singleBook2,
     required this.singleBookEvent,
-    required this.singleBookEventComment,
-    // required this.singleBook2,
+    required this.singleBookComment,
   });
 
   @override
@@ -40,9 +41,7 @@ class _EventTileState extends State<EventTile> {
           children: [
             Container(
               decoration: BoxDecoration(
-
                 color: Theme.of(context).colorScheme.onInverseSurface,
-
                 borderRadius: BorderRadius.circular(8),
               ),
               margin: EdgeInsets.only(top: 5, left: 25, right: 25, bottom: 10),
@@ -65,7 +64,7 @@ class _EventTileState extends State<EventTile> {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
-                          'Released by ${widget.singleBookEvent.name}',
+                          '${widget.singleBookEvent.event} ${widget.singleBookEvent.username}',
                           style: TextStyle(color: Colors.grey[500]),
                         ),
                         Text(
@@ -77,14 +76,14 @@ class _EventTileState extends State<EventTile> {
                           height: 5,
                         ),
                         Text(
-                          widget.singleBookEvent.note,
+                          widget.singleBookEvent.userNote,
                           textAlign: TextAlign.left,
                         ),
                         SizedBox(
                           height: 15,
                         ),
                         Text(
-                          widget.singleBookEventComment.userName,
+                          widget.singleBookComment.username,
                           style: TextStyle(
                             color: Colors.grey[500],
                           ),
@@ -93,7 +92,7 @@ class _EventTileState extends State<EventTile> {
                           height: 5,
                         ),
                         Text(
-                          widget.singleBookEventComment.commentBody,
+                          widget.singleBookComment.commentsBody,
                         ),
                         const SizedBox(
                           height: 20,
@@ -108,92 +107,8 @@ class _EventTileState extends State<EventTile> {
         ),
       );
     }
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              color: Theme.of(context).colorScheme.onInverseSurface,
-              borderRadius: BorderRadius.circular(8),
-            ),
-            margin: EdgeInsets.only(top: 5, left: 25, right: 25, bottom: 10),
-            padding: EdgeInsets.all(25),
-            child: Row(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.grey[400],
-                  ),
-                  padding: EdgeInsets.all(10),
-                  margin: EdgeInsets.only(
-                    right: 15,
-                  ),
-                  child: const Icon(Icons.person),
-                ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Released by ${widget.singleBookEvent.name}',
-                        style: TextStyle(color: Colors.grey[500]),
-                      ),
-                      Text(
-                        timeago.format(
-                            DateTime.parse(widget.singleBookEvent.timestamp)),
-                        style: TextStyle(color: Colors.grey[500]),
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      Text(
-                        widget.singleBookEvent.note,
-                        textAlign: TextAlign.left,
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      Text(
-                        widget.singleBookEventComment.userName,
-                        style: TextStyle(
-                          color: Colors.grey[500],
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      Text(
-                        widget.singleBookEventComment.commentBody,
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      TextField(
-                          style: TextStyle(fontSize: 15),
-                          onChanged: (text) {
-                            print(text);
-                          },
-                          decoration: InputDecoration(
-                            suffixIcon: IconButton(
-                              onPressed: () {},
-                              icon: Icon(Icons.send),
-                            ),
-                            hoverColor: Colors.blue[900],
-                            hintText: 'Post a comment',
-                            border: const OutlineInputBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(15.0))),
-                          )),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
-        ],
-      ),
+    return Container(
+      child: Text('blah'),
     );
   }
 }
